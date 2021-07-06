7cf20bcd05ba8170c157fd46f620876a035b1020
zhoubin <chirillo@163.com>
2021-07-07 00:11:23 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+/**
+ * 项目的启动入口
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式文件
+import './styles/index.less'
+
+// 关闭生产环境提示
 Vue.config.productionTip = false
 
+// 创建vue根实例 配置router
 new Vue({
   router,
   render: h => h(App)
+  // el: 'app' 等价于 $mount('#app')
 }).$mount('#app')
